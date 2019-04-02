ef0cb7cadf0a452ea7a2523026cd13fc175baf40
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-04-02 18:44:23 +0200

    Created golden acorn app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,61 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({inputString, inputFunc}) => {
+
+  const element = (
+    <div>
+      <button onClick={inputFunc}>{inputString}</button>
+    </div>
+  );
+
+  return element; 
+};
+
+const Display = ({inputNum}) => {
+  const element = (
+    <div>
+      <h1>{inputNum}</h1>
+    </div>
+  );
+
+  return element;
+};
+
+class GoldenAcornApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {counter: 0};
+    this.buyOne = this.buyOne.bind(this);
+    this.eatOne = this.eatOne.bind(this); /* EZ mi? */
+  };
+
+  buyOne () {
+    this.setState(previousState => {
+      return {counter: previousState.counter + 1};
+    });
+  };
+
+  eatOne () {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+      <Button inputString={'Buy one'} inputFunc={this.buyOne}/>
+      <Display inputNum={this.state.counter}/>
+      <Button inputString={'Eat one'} inputFunc={this.eatOne}/>
+      </div>
+    );
+  }
+};
+ 
 ReactDOM.render(
-  <h1>Hello, Green Fox Academy!</h1>,
-  document.getElementById('root'),
+  <GoldenAcornApp />, document.getElementById('root')
 );
