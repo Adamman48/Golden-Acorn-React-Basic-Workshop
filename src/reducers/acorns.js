c0c4660c45d61c1ad757d13d10a06322cb5c273f
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-04-04 11:41:52 +0200

    Added initialstate and initialized acorns reducer. Added case when no action happens, adding full functionality now.

--- src/reducers/acorns.js
+++ src/reducers/acorns.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const initialState = {
+  acorns: 0,
+};
 
+const acorns = (state = initialState, action) => {
+  typeof state === undefined ?
+    initialState :
+  null
+};
 
 export { acorns };
