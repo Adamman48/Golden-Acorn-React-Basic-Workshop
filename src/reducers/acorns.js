86a813947dba5894e7efc0dd4cb4728d0d209d33
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-04-08 10:55:01 +0200

    Replaced the ternary operator with Math.max which blocked the count when the counter reached zero.

--- src/reducers/acorns.js
+++ src/reducers/acorns.js
@@ -11,16 +11,12 @@ const acorns = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_ACORN':
       return Object.assign({}, state, {
-      //...state,
         acornCount: state.acornCount + action.incrementBy,
         ID: state.ID + 1
       });
     case 'SUBTRACT_ACORN':
       return Object.assign({}, state, {
-      //...state,
-        acornCount: state.acornCount > 0 ? // OK like this?
-          state.acornCount - action.decrementBy : 
-        initialState.acornCount,
+        acornCount: Math.max(initialState.acornCount, 0),
         ID: state.ID + 1
       });
     default:
